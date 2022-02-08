a1e06c1e51ee3ed186645b2f3720fd989dc5ffcd
Anton Klemen <antonklemen@gmail.com>
2022-02-08 22:30:21 +0100

    Fix file names

--- src/components/Screenshot.tsx
+++ src/components/Screenshot.tsx
@@ -13,6 +13,7 @@ import React, { useState } from "react";
 import { ReactComponent as ScreenshotSvg } from "../assets/img_screen.svg";
 import { GOOGLE_DRIVE_SCREENSHOTS_URL, sites } from "../constants";
 import { useAuth, useToast } from "../contexts/App";
+import { Site } from "../interfaces/Site";
 import { getScreenshot } from "../services/api/getScreenshot";
 import { uploadFileToDrive } from "../services/api/uploadFileToDrive";
 
@@ -20,23 +21,19 @@ function Screenshot() {
     const { apiKey, getValidGoogleCredentials } = useAuth();
     const showToast = useToast();
     const [loading, setLoading] = useState<boolean>(false);
-    const [siteUrl, setSiteUrl] = useState<string>(sites[0].url);
+    const [site, setSite] = useState<Site>(sites[0]);
 
     const handleScreenshotFlow = async () => {
         setLoading(true);
         try {
             if (apiKey) {
-                if (!siteUrl) {
-                    showToast("Please choose a website!");
-                    return;
-                }
-                const screenshot = await getScreenshot(siteUrl, apiKey);
+                const screenshot = await getScreenshot(site.url, apiKey);
                 const accessToken = await getValidGoogleCredentials();
                 if (accessToken) {
                     await uploadFileToDrive(
                         screenshot,
                         accessToken.token,
-                        siteUrl
+                        site.id + "_" + site.name + ".jpg"
                     );
                     showToast(
                         "The screenshot was successfully uploaded to Google Drive!"
@@ -58,7 +55,12 @@ function Screenshot() {
     };
 
     const handleSiteChange = (e: SelectChangeEvent) => {
-        setSiteUrl(e.target.value as string);
+        const site: Site | undefined = sites.find(
+            (site) => site.id.toString() === e.target.value
+        );
+        if (site) {
+            setSite(site);
+        }
     };
 
     return (
@@ -77,12 +79,12 @@ function Screenshot() {
                     <Select
                         labelId="select-label"
                         id="select"
-                        value={siteUrl}
                         label="Website"
+                        value={site.id.toString()}
                         onChange={(e) => handleSiteChange(e)}
                     >
                         {sites.map((site) => (
-                            <MenuItem key={site.id} value={site.url}>
+                            <MenuItem key={site.id} value={site.id.toString()}>
                                 {site.name}
                             </MenuItem>
                         ))}
